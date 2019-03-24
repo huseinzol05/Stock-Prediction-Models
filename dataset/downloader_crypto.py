229c53ee16c8c8ddbeb612ea5fd7c2d5aced57f6
TensorTom <tomashley@protonmail.com>
2019-03-24 09:12:38 -0400

    Create downloader_crypto.py

---
+++ dataset/downloader_crypto.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+
+# usage: python downloader_crypto.py bitmex BTC/USD 2015-01-01T00:00:00Z 1d btc.csv
+
+import os
+import sys
+from sys import argv
+import csv
+from pprint import pprint
+from datetime import datetime
+
+# -----------------------------------------------------------------------------
+
+root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(root + '/python')
+
+import ccxt  # noqa: E402
+
+
+# -----------------------------------------------------------------------------
+
+def retry_fetch_ohlcv(exchange, max_retries, symbol, timeframe, since, limit):
+    num_retries = 0
+    try:
+        num_retries += 1
+        ohlcv = exchange.fetch_ohlcv(symbol, timeframe, since) # limit
+        # print('Fetched', len(ohlcv), symbol, 'candles from', exchange.iso8601 (ohlcv[0][0]), 'to', exchange.iso8601 (ohlcv[-1][0]))
+        return ohlcv
+    except Exception:
+        if num_retries > max_retries:
+            raise  # Exception('Failed to fetch', timeframe, symbol, 'OHLCV in', max_retries, 'attempts')
+
+
+def scrape_ohlcv(exchange, max_retries, symbol, timeframe, since, limit):
+    earliest_timestamp = exchange.milliseconds()
+    timeframe_duration_in_seconds = exchange.parse_timeframe(timeframe)
+    timeframe_duration_in_ms = timeframe_duration_in_seconds * 1000
+    timedelta = limit * timeframe_duration_in_ms
+    all_ohlcv = []
+    while True:
+        fetch_since = earliest_timestamp - timedelta
+        ohlcv = retry_fetch_ohlcv(exchange, max_retries, symbol, timeframe, fetch_since, limit)
+        # if we have reached the beginning of history
+        if ohlcv[0][0] >= earliest_timestamp:
+            break
+        earliest_timestamp = ohlcv[0][0]
+        all_ohlcv = ohlcv + all_ohlcv
+        print(len(all_ohlcv), 'candles in total from', exchange.iso8601(all_ohlcv[0][0]), 'to', exchange.iso8601(all_ohlcv[-1][0]))
+        # if we have reached the checkpoint
+        if fetch_since < since:
+            break
+    return all_ohlcv
+
+
+def write_to_csv(filename, data):
+    with open(filename, mode='w') as output_file:
+        output_file.write("Date,Open,High,Low,Close,Volume\n")
+        csv_writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerows(data)
+
+
+def scrape_candles_to_csv(filename, exchange_id, max_retries, symbol, timeframe, since, limit):
+    # instantiate the exchange by id
+    exchange = getattr(ccxt, exchange_id)({
+        'enableRateLimit': True,  # required by the Manual
+    })
+    # convert since from string to milliseconds integer if needed
+    if isinstance(since, str):
+        since = exchange.parse8601(since)
+    # preload all markets from the exchange
+    exchange.load_markets()
+    # fetch all candles
+    ohlcv = scrape_ohlcv(exchange, max_retries, symbol, timeframe, since, limit)
+
+    key = 0
+    for candle in ohlcv:
+        epoch = int(candle[0]) / 1000
+        ohlcv[key][0] = datetime.utcfromtimestamp(epoch).strftime('%Y-%m-%d') #  %H:%M:%S
+        ohlcv[key][5] = int(candle[5])
+        key += 1
+
+    # save them to csv file
+    write_to_csv(filename, ohlcv)
+    print('Saved', len(ohlcv), 'candles from', exchange.iso8601(ohlcv[0][0]), 'to', exchange.iso8601(ohlcv[-1][0]), 'to', filename)
+
+
+# -----------------------------------------------------------------------------
+
+exchange = argv[1]
+symbol = argv[2]
+start_date = argv[3]
+timeframe = argv[4]
+outfile = argv[5]
+
+
+scrape_candles_to_csv(outfile, exchange, 3, symbol, timeframe, start_date, 100)
